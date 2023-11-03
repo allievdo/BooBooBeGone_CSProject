9e637fea4f2ff94972b4a0bf2153f5029594e59a
allikitten <allison.evdokimo@cox.net>
2023-11-03 13:12:54 -0700

    Stuff

--- Boo Boo Be Gone - CS225 Group Project/Assets/Scripts/Ghost.cs
+++ Boo Boo Be Gone - CS225 Group Project/Assets/Scripts/Ghost.cs
@@ -8,25 +8,58 @@ public class Ghost : MonoBehaviour
     public int curHP = 100;
 
     public int attackPower;
+    public float ghostSpeed = 2;
+
+    public bool hasCollided = false;
+
+    public Rigidbody2D rb;
 
     public PlayerMovement playerMovement;
 
     //SpriteRenderer spriteRenderer;
 
+    public void Update()
+    {
+        GhostMovement();
+
+        if (curHP == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
+        if(collision.gameObject.tag == ("Wall"))
+        {
+            hasCollided = true;
+            Debug.Log("Collided");
+        }
+
+        if (collision.gameObject.tag == ("Wall2"))
+        {
+            hasCollided = false;
+        }
+
         if (collision.gameObject.tag == ("Player"))
         {
             curHP -= 10;
             playerMovement.playerCurHP -= 5;
             Debug.Log("Hit");
         }
+
+        //else
+        //{
+        //    hasCollided = false;
+        //}
     }
-    public void Update()
+    private void GhostMovement()
     {
-        if (curHP == 0)
+        rb.velocity = new Vector2(ghostSpeed, 0f);
+
+        if (hasCollided == true)
         {
-            Destroy(gameObject);
+            rb.velocity = new Vector2(-ghostSpeed, 0f);
         }
     }
 }
